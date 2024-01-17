dba1fa84bb06807495342e8b9da494da6d02437d
Bharat Shira <shirab@sheridancollege.ca>
2024-01-17 09:45:18 -0500

    First commit

---
+++ src/arraydemo/ArrayDemo.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+package arraydemo;
+
+/**
+ * Date - Jan 10,2024
+ * @author bharat
+ */
+public class ArrayDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Enter a word");
+        Scanner input = new Scanner(System.in);
+        
+        String myWord = input.nextLine();
+        
+        char[] myLetters = new char[myWord.length()];
+        
+        for (int i = 0; i<myLetters.length; i++){
+             myLetters[i] = myWord.charAt(i);
+             }
+        
+        System.out.println("Printing in reverse");
+        
+    }
+    
+}
