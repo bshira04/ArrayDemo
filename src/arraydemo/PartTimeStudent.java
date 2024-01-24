3cbc97402a39d7d3cffa374a79e05c4f663b90b1
Bharat Shira <shirab@sheridancollege.ca>
2024-01-24 10:21:57 -0500

    Added part time student class

---
+++ src/arraydemo/PartTimeStudent.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arraydemo;
+
+/**
+ *
+ * @author bharat
+ */
+public class PartTimeStudent extends ArrayDemo {
+    private int numOfCourses;
+
+    public PartTimeStudent(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+    
+}
